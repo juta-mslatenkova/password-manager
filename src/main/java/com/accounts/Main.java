a0f9a30e3c17d70bf18898d038efd625141f2311
Juta Mischenko-Slatenkova <juta.mischenko@gmail.com>
2019-04-01 22:44:42 +0300

    Hibernate connection to PostgreSQL created

---
+++ src/main/java/com/accounts/Main.java
@@ -0,0 +1,4 @@
+package com.accounts;
+
+public class Main {
+}
---
+++ src/main/java/com/accounts/model/Account.java
@@ -0,0 +1,8 @@
+package com.accounts.model;
+
+/**
+ * Model class - a POJO containing get/set methods to store data retrieved using DAOImpl class
+ * + this in entity class, one which maps data objects with the database
+ */
+public class User {
+}
